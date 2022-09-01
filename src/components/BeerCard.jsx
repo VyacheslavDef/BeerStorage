95f33f976356899bbdf0c2c864c44d1f7314df16
Вячеслав Четвертаков <101511080+VyacheslavDef@users.noreply.github.com>
2022-09-02 00:14:20 +0300

    favorit mark

--- src/components/BeerCard.jsx
+++ src/components/BeerCard.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const BeerCard = (props) => {
+const BeerCard = ({ beerPost, change }) => {
   const shortDesc = (() => {
-    if (props.beerPost.description.length > 200) {
-      return props.beerPost.description.slice(0, 200) + "...";
+    if (beerPost.description.length > 200) {
+      return beerPost.description.slice(0, 200) + "...";
     }
-    return props.beerPost.description;
+    return beerPost.description;
   })();
 
   return (
-
-      <div className="card mb-5">
-        <div className="row">
-          <div className="col-md-4 card-left-side">
-            <img
-              src={props.beerPost.image_url}
-              className="img-card"
-              alt=""
-            ></img>
-          </div>
-          <div className="col-md-8 card-right-side">
-            <div className="card-body">
-              <h2 className="card-title">{props.beerPost.name}</h2>
-              <p className="card-text">{shortDesc}</p>
-              <div className="card-button">
-                <button type="button" className="btn btn-outline-dark btn-lg">
-                  <Link className="a" to={"/test-on-react/beers/" + props.beerPost.id}>
-                    Подробнее
-                  </Link>
-                </button>
-              </div>
+    <div className="card mb-5">
+      <div className="row">
+        <div className="col-md-4 card-left-side">
+          <img src={beerPost.image_url} className="img-card" alt=""></img>
+        </div>
+        <div className="col-md-8 card-right-side">
+          <div className="card-body">
+            <div className="card-body-top">
+              <h2 className="card-title">{beerPost.name}</h2>
+              <i
+                class={"bi bi-heart" + (beerPost.favorites ? "-fill" : "")}
+                onClick={() => change(beerPost.id)}
+              ></i>
+            </div>
+            <p className="card-text">{shortDesc}</p>
+            <div className="card-button">
+              <button type="button" className="btn btn-outline-dark btn-lg">
+                <Link className="a" to={"/test-on-react/beers/" + beerPost.id}>
+                  Подробнее
+                </Link>
+              </button>
             </div>
           </div>
         </div>
       </div>
-
+    </div>
   );
 };
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -12,7 +12,7 @@ function MainPage() {
 
   const [searchBeer, setSearchBeer] = useState("");
 
-  const {per_page, current_page} = useParams() 
+  const { per_page, current_page } = useParams();
 
   const [totalCount] = useState(325);
   const [totalPages, setTotalPages] = useState(0);
@@ -20,15 +20,10 @@ function MainPage() {
   const [perPage] = useState(per_page);
 
 
+
   const filtersBeer = beerPost.filter((beer) => {
     return beer.name.toLowerCase().includes(searchBeer.toLowerCase());
   });
-
-  useEffect(() => {
-    beerPosts(page, perPage);
-  },[page, perPage] );
-
-
 
   async function beerPosts(page, perPage) {
     const responce = await PostService.getAll(page, perPage);
@@ -36,33 +31,48 @@ function MainPage() {
     setTotalPages(getPages(totalCount, perPage));
   }
 
-  if (!beerPost.length) {
-    return <Loader/>;
-  }
+  useEffect(() => {
+    beerPosts(page, perPage);
+  }, [page, perPage]);
 
   const changePage = (page, per_page) => {
     setPage(page);
     beerPosts(page, per_page);
   };
 
-  return (
+  const change = (id) => {
+   const newarr = beerPost.map((faw) => {
+      if (faw.id === id) {
+      return { ...faw, favorites: !faw.favorites}
+      }
+      return faw
+    })
+    setBeerPost(newarr)
+  };
+
+  return beerPost.length ? (
     <section className="main_page">
-    <div className="container">
-      <h1>BEERS STORAGE</h1>
-      <input
-      className="mb-5"
-        placeholder="Search..."
-        value={searchBeer}
-        onChange={(e) => setSearchBeer(e.target.value)}
-      ></input>
-      {filtersBeer.map((beerPost) => (
-        <BeerCard beerPost={beerPost} key={beerPost.id}/>
-      ))}
+      <div className="container">
+        <h1>BEERS STORAGE</h1>
+        <input
+          className="mb-5"
+          placeholder="Search..."
+          value={searchBeer}
+          onChange={(e) => setSearchBeer(e.target.value)}
+        ></input>
+        {filtersBeer.map((beerPost) => (
+          <BeerCard beerPost={beerPost} key={beerPost.id} change={change} />
+        ))}
 
-        <Pagination page={page} totalPages={totalPages} changePage={changePage}/>
-
-    </div>
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          changePage={changePage}
+        />
+      </div>
     </section>
+  ) : (
+    <Loader />
   );
 }
 
