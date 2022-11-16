c435da9b138bb8ead0902a75dfea67ff7b9fbf38
Вячеслав Четвертаков <101511080+VyacheslavDef@users.noreply.github.com>
2022-11-16 23:08:18 +0300

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header'>
-      <ul>
-        <li><Link to={'/test-on-react'}>Главная</Link></li>
-        <li><Link to={'/test-on-react/faworits'}>Избранное</Link></li>
-      </ul>
+    <div className="header">
+      <div className="header_left"></div>
+      <div className="header_center">
+        <ul className="header_center_items">
+          <li>
+            <Link to={"/test-on-react"}>Главная</Link>
+          </li>
+          <li>
+            <Link to={"/test-on-react/faworits"}>Избранное</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="header_right"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/FawPage.jsx
+++ src/pages/FawPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const FawPage = ({ faw }) => {
   return (
+    <div className='container'>
     <h1>Faworits</h1>
+    </div>
   )
 }
 
